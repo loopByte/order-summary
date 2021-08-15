567ccb691ad73657d32c480053b94d06f17fffad
Vasile Milea <milea.vasile959@gmail.com>
2021-08-15 11:18:27 +0000

    Added component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <OrderSummary />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OrderSummary from './components/OrderSummary.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    OrderSummary
   }
 }
 </script>
---
+++ src/components/OrderSummary.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="order">
+
+    </div>
+</template>
+
+<style scoped>
+* {
+    padding: 0;
+    margin: 0;
+
+    box-sizing: border-box;
+}
+</style>
